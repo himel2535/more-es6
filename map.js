022bf0066b444ee2eb0add58706916223f86eacf
himel2535 <monwarhossanhimel@gmail.com>
2025-08-31 02:54:39 +0600

    map on array and string

---
+++ map.js
@@ -0,0 +1,56 @@
+// const numbers=[2,10,50,30]
+
+// const doubled=[];
+// for (number of numbers){
+//     const result=number*2;
+//     doubled.push(result)
+// }
+// console.log(doubled)
+
+
+// ----now we will apply map for same work---
+
+// const numbers=[2,10,50,30];
+// const doubleIt=x=>x*2;
+// const double=numbers.map(doubleIt);
+// console.log(double)
+
+// ----there the direct function is sitting in map directly just--->
+
+// const numbers=[2,10,50,30];
+// const doubled=numbers.map(x=>x*2);
+// console.log(doubled)
+
+
+// const numbers=[2,10,50,30];
+// console.log(numbers.map(x=>x*2))
+
+
+// ----map on string---->
+
+friends=['himel','monwar','hossan'];
+// length of array-->
+const lengthFriendName=friends.map(name=>name.length)
+// first letter of name-->
+const firstLetter=friends.map(name=>name[0].toUpperCase())
+
+// console.log(lengthFriendName)
+// console.log(firstLetter)
+
+
+
+// ---By default it will show us undefined if consol log give outside for multiple line --->
+numbers=[2,10,50,30];
+const result=numbers.map((num,index)=>{ 
+    // const total=num*index
+    // console.log(total)
+    console.log(num*index)
+})
+console.log(result)
+
+
+numbers2=[2,10,50,30];
+const result2=numbers2.map((num,index)=>{ 
+    return num*index;
+})
+console.log(result2)
